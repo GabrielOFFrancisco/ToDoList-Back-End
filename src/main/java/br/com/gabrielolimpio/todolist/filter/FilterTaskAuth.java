3184c9f9fe05618c715cb94bb3a3178f3c3d0861
Gabriel <gabriel.offrancisco@gmail.com>
2023-10-13 14:21:02 -0300

    Atualizando tarefas e validação de rotas

--- src/main/java/br/com/gabrielolimpio/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/gabrielolimpio/todolist/filter/FilterTaskAuth.java
@@ -15,7 +15,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth extends OncePerRequestFilter{
+public class FilterTaskAuth extends OncePerRequestFilter {
 
     @Autowired
     private IUserRepository userRepository;
@@ -24,35 +24,43 @@ public class FilterTaskAuth extends OncePerRequestFilter{
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-                // coletar autenticação
-                var authorization = request.getHeader("Authorization");
+        var servletPath = request.getServletPath();
 
-                var authEncoded = authorization.substring("Basic".length()).trim();
-                
-                byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+        System.out.println(servletPath);
+        if (servletPath.startsWith("/tasks/")) {
 
-                var authString = new String(authDecoded);
+            // coletar autenticação
+            var authorization = request.getHeader("Authorization");
 
-                String[] credentials = authString.split(":");
-                String username = credentials[0];
-                String password = credentials[1];
+            var authEncoded = authorization.substring("Basic".length()).trim();
 
-                //validar usuário
-                var user = this.userRepository.findByUsername(username);
+            byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
 
-                if(user == null){
-                    response.sendError(401 );
-                } else{
-                    // validar senha
-                    var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            var authString = new String(authDecoded);
 
-                    if(passwordVerify.verified){
-                        filterChain.doFilter(request, response);
-                    } else{
-                        response.sendError(401);
-                    }
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+
+            // validar usuário
+            var user = this.userRepository.findByUsername(username);
+
+            if (user == null) {
+                response.sendError(401);
+            } else {
+                // validar senha
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+                if (passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
                 }
             }
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
 
-    
 }
